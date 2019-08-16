cf04c4614fc9b1ff1c91c138a10116bce0aa446c
Divyam Singh <divyamsingh13@gmail.com>
2019-08-16 16:32:29 +0530

    AccountBranchPojo Added

--- src/main/java/com/corebanking/spring/model/Customer.java
+++ src/main/java/com/corebanking/spring/model/Customer.java
@@ -12,12 +12,24 @@ public class Customer
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
-	private String accountno;
-	private long balance;
 	private String phone;
 	private String address;
 	private String email;
 	private long salary;
+	private String username;
+	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public int getId() {
 		return id;
 	}
@@ -30,18 +42,7 @@ public class Customer
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getAccountno() {
-		return accountno;
-	}
-	public void setAccountno(String accountno) {
-		this.accountno = accountno;
-	}
-	public long getBalance() {
-		return balance;
-	}
-	public void setBalance(long balance) {
-		this.balance = balance;
-	}
+	
 	public String getPhone() {
 		return phone;
 	}
