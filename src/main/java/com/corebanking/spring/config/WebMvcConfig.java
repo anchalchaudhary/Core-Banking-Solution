7a97e313383d0e6d1eb2a4b2707ff885e71923b6
Divyam Singh <divyamsingh13@gmail.com>
2019-08-19 17:00:08 +0530

    Bug fixes

--- src/main/java/com/corebanking/spring/config/WebMvcConfig.java
+++ src/main/java/com/corebanking/spring/config/WebMvcConfig.java
@@ -1,18 +1,26 @@
 package com.corebanking.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.corebanking.spring")
-public class WebMvcConfig implements WebMvcConfigurer{
+public class WebMvcConfig {
 
-	@Override
-	public void configureViewResolvers(ViewResolverRegistry registry) {
-		registry.jsp().prefix("/WEB-INF/views/").suffix(".jsp");
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+
+		// set location of views.
+		vr.setPrefix("/WEB-INF/views/");
+
+		// set the extension of views.
+		vr.setSuffix(".jsp");
+
+		return vr;
 	}
 }
